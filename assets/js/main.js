136c9839fe2ca33a1212e4d9cf1fdf938e0b2544
naybala <naybalaismylife.nbl@gmail.com>
2022-03-29 18:19:28 +0630

    resolving bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,56 @@
-// Adding Theme Mode On Off
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+//Changing Bg Color Profile
+document.querySelectorAll('.theme-colors .color').forEach(color => {
+    color.onclick = () => {
+        let background = color.style.background;
+        document.querySelector(':root').style.setProperty('--first-color', background);
+    }
+});
+
+
+
+
 let themeChanger = document.querySelector('.theme-changer');
 let bgContainer = document.querySelector('.bg-container');
 themeChanger.onclick = () => {
@@ -31,67 +83,4 @@ themeToggler.onclick = () => {
 
 }
 
-//Changing Bg Color Profile
-document.querySelectorAll('.theme-colors .color').forEach(color => {
-    color.onclick = () => {
-        let background = color.style.background;
-        document.querySelector(':root').style.setProperty('--first-color', background);
-    }
-});
 
-/*===== MENU SHOW =====*/
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
-
-    if (toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show')
-        })
-    }
-}
-showMenu('nav-toggle', 'nav-menu')
-
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction() {
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive() {
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
-
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        } else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
-
-/*===== SCROLL REVEAL ANIMATION =====*/
-// const sr = scrollActive({
-//     origin: 'top',
-//     distance: '60px',
-//     duration: 1000,
-//     delay: 200,
-//     //     reset: true
-// });
-
-// sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
-// sr.reveal('.home__img, .about__subtitle', { delay: 400 });
-// sr.reveal('.home__social-icon', { interval: 200 });
-// sr.reveal('.skills__data, .container .card, .contact__input', { interval: 100 });
