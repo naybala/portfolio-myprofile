e081976294e9d1226747f84080aa5ff9a4aef742
naybala <naybalaismylife.nbl@gmail.com>
2022-03-30 14:39:18 +0630

    finishing

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,6 @@ const themeColor = document.querySelectorAll('.theme-colors .color');
 themeColor.forEach(color => {
     color.onclick = () => {
         let background = color.style.background;
-        console.log(background);
         document.querySelector(':root').style.setProperty('--first-color', background);
     }
 });
@@ -71,28 +70,134 @@ themeToggler.onclick = () => {
 }
 
 const accept = localStorage.getItem('accepted');
-const dark = localStorage.getItem('dark')
+const dark = localStorage.getItem('dark');
+
 if (accept == 1) {
+    // Adding Theme Mode Using local Storage
+    themeColor.forEach(color => {
+        color.onclick = () => {
+            let background = color.style.background;
+            switch (background) {
+                case 'rgb(62, 111, 244)':
+                    localStorage.setItem("theme", "1");
+                    break;
+                case 'rgb(0, 68, 253)':
+                    localStorage.setItem("theme", "2");
+                    break;
+                case 'rgb(39, 174, 96)':
+                    localStorage.setItem("theme", "3");
+                    break;
+                case 'rgb(0, 238, 99)':
+                    localStorage.setItem("theme", "4");
+                    break;
+
+                case 'rgb(15, 185, 177)':
+                    localStorage.setItem("theme", "5");
+                    break;
+                case 'rgb(0, 255, 242)':
+                    localStorage.setItem("theme", "6");
+                    break;
+                case 'rgb(206, 178, 68)':
+                    localStorage.setItem("theme", "7");
+                    break;
+                case 'rgb(255, 204, 0)':
+                    localStorage.setItem("theme", "8");
+                    break;
+
+                case 'rgb(172, 53, 76)':
+                    localStorage.setItem("theme", "9");
+                    break;
+                case 'rgb(255, 0, 51)':
+                    localStorage.setItem("theme", "10");
+                    break;
+                case 'rgb(232, 67, 147)':
+                    localStorage.setItem("theme", "11");
+                    break;
+                case 'rgb(186, 238, 0)':
+                    localStorage.setItem("theme", "12");
+                    break;
+                case 'rgb(145, 75, 108)':
+                    localStorage.setItem("theme", "13");
+                    break;
+                case 'rgb(252, 7, 239)':
+                    localStorage.setItem("theme", "14");
+                    break;
+                default:
+                    break;
+            }
+            console.log(background);
+            document.querySelector(':root').style.setProperty('--first-color', background);
+        }
+    });
+    const theme = localStorage.getItem('theme');
+
+    switch (theme) {
+        case "1":
+            document.querySelector(':root').style.setProperty('--first-color', 'rgb(62, 111, 244)');
+            break;
+        case "2":
+            document.querySelector(':root').style.setProperty('--first-color', 'rgb(0, 68, 253)');
+            break;
+        case "3":
+            document.querySelector(':root').style.setProperty('--first-color', 'rgb(39, 174, 96)');
+            break;
+        case "4":
+            document.querySelector(':root').style.setProperty('--first-color', 'rgb(0, 238, 99)');
+            break;
+
+        case "5":
+            document.querySelector(':root').style.setProperty('--first-color', 'rgb(15, 185, 177)');
+            break;
+        case "6":
+            document.querySelector(':root').style.setProperty('--first-color', 'rgb(0, 255, 242)');
+            break;
+        case "7":
+            document.querySelector(':root').style.setProperty('--first-color', 'rgb(206, 178, 68)');
+            break;
+        case "8":
+            document.querySelector(':root').style.setProperty('--first-color', 'rgb(255, 204, 0))');
+            break;
+        case "9":
+            document.querySelector(':root').style.setProperty('--first-color', 'rgb(172, 53, 76)');
+            break;
+        case "10":
+            document.querySelector(':root').style.setProperty('--first-color', 'rgb(255, 0, 51)');
+            break;
+        case "11":
+            document.querySelector(':root').style.setProperty('--first-color', 'rgb(232, 67, 147)');
+            break;
+        case "12":
+            document.querySelector(':root').style.setProperty('--first-color', 'rgb(186, 238, 0)');
+            break;
+
+        case "13":
+            document.querySelector(':root').style.setProperty('--first-color', 'rgb(145, 75, 108)');
+            break;
+        case "14":
+            document.querySelector(':root').style.setProperty('--first-color', 'rgb(252, 7, 239)');
+            break;
+        default:
+            break;
+    }
+
+
+
+    // Adding Dark Mode Using local Storage
     themeToggler.onclick = () => {
         themeToggler.classList.toggle('activeToggle');
         if (themeToggler.classList.contains('activeToggle')) {
             document.body.classList.add('activeDark');
             headDark.classList.add('ac');
             cookiee.classList.add('dark');
-            localStorage.removeItem('dark', '0')
             localStorage.setItem("dark", "1");
         } else {
             document.body.classList.remove('activeDark');
             headDark.classList.remove('ac');
             cookiee.classList.remove('dark');
-            localStorage.removeItem('dark', '1')
             localStorage.setItem("dark", "0");
         }
 
     }
-
-
-
     if (dark == 1) {
         themeToggler.classList.toggle('activeToggle');
         document.body.classList.add('activeDark');
