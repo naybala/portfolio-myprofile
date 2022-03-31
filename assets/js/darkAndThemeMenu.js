6b148b6b31a1aef6ca41a1ee5084de26d8e47e9a
naybala <naybalaismylife.nbl@gmail.com>
2022-03-31 14:51:47 +0630

    commit

--- assets/js/darkAndThemeMenu.js
+++ assets/js/darkAndThemeMenu.js
@@ -2,11 +2,13 @@ let theme = document.querySelector('.themes-container');
 let themeOpen = document.querySelector('#theme-open');
 
 themeOpen.onclick = () => {
+    console.log("open");
     theme.classList.add('activeMenu');
     themeOpen.style.opacity = '0';
 }
 
 document.querySelector('#theme-close').onclick = () => {
+    console.log("close");
     theme.classList.remove('activeMenu');
     themeOpen.style.opacity = '1';
 }
